2de3a22b904410a4ba2c43d85aab4b2662e980a0
proehlen <peter.roehlen@gmail.com>
2023-07-16 10:27:14 +0700

    Switch to use-package

--- init.el
+++ init.el
@@ -1,44 +1,43 @@
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
+
+(use-package evil
+    :init
+    (evil-mode 1))
+
+(use-package powerline
+  :init
+  (powerline-default-theme))
+  
+
+;; Use relative line numbers
+(setq display-line-numbers-type 'relative) 
+;; activate line numbering in all buffers/modes
+(global-display-line-numbers-mode) 
+
+;;;; Download Vertico
+;;(unless (package-installed-p 'vertico)
+;;  (package-install 'vertico))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(tango-dark))
- '(package-selected-packages '(vertico linum-relative powerline evil)))
+ '(package-selected-packages '(use-package vertico linum-relative powerline evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-
-;; Enable Evil
-(require 'evil)
-(evil-mode 1)
-
-;; Download Powerline
-(unless (package-installed-p 'powerline)
-  (package-install 'powerline))
-
-;; Activate Powerline
-(powerline-default-theme)
-
-;; Use relative line numbers
-;; set type of line numbering (global variable)
-(setq display-line-numbers-type 'relative) 
-
-;; activate line numbering in all buffers/modes
-(global-display-line-numbers-mode) 
-
-;; Download Vertico
-(unless (package-installed-p 'vertico)
-  (package-install 'vertico))
-
