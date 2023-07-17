a6939315bdee1e4b957ec208c3760cd5bcf38376
proehlen <peter.roehlen@gmail.com>
2023-07-17 09:45:50 +0700

    Use modus theme

--- init.el
+++ init.el
@@ -42,13 +42,17 @@
   :init
   (marginalia-mode))
 
+;; Use preferred theme
+(use-package modus-themes
+  :ensure t
+  :init
+  (load-theme 'modus-vivendi-tinted :no-confirm))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(misterioso))
  '(package-selected-packages '(use-package vertico linum-relative powerline evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
